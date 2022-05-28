2902234f962813a717be4407ee0c14fbf6e7f86a
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-05-28 00:31:54 -0400

    Building view by methods

--- query.js
+++ query.js
@@ -12,64 +12,152 @@ const db = mysql.createConnection(
   );
 
 class Query{
-    constructor(){};
-    displayTable(info){
-      let table = cTable.getTable(info[0]);
-      console.log('\n');
-      console.log(table);  
-    }
-     async viewAllDepartments(){
-        let view = await db.promise().query('SELECT departments.dep_name AS Department, departments.id AS ID FROM departments');
-        this.displayTable(view);     
-    }
-    async viewAllRoles(){
-        let view = await db.promise().query(`SELECT roles.title AS 'Job Title', roles.id AS ID, departments.dep_name AS Department, roles.salary as Salary
-        FROM roles
-        LEFT JOIN departments
-        ON roles.dep_id = departments.id;`);
-        this.displayTable(view);;       
-    }
-    async viewAllEmployees(){
-      let view = await db.promise().query(`SELECT employees.id AS ID, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', roles.title AS 'Job Title', departments.dep_name AS Department, roles.salary as Salary, concat(managers.first_name,' ',managers.last_name) AS Manager
-      FROM EMPLOYEES
-      LEFT JOIN roles
-      ON employees.role_id = roles.id
-      LEFT JOIN departments
-      ON roles.dep_id = departments.id
-      LEFT JOIN employees AS managers ON employees.manager_id = managers.id;`);
-      this.displayTable(view);      
+  constructor(){};
+  displayTable(info){
+    let table = cTable.getTable(info[0]);
+    console.log('\n');
+    console.log(table);  
   }
-  async addNew(element,response){
+    async viewAllDepartments(){
+    let view = await db.promise().query('SELECT departments.dep_name AS Department, departments.id AS ID FROM departments');
+    this.displayTable(view);     
+  }
+  async viewAllRoles(){
+    let view = await db.promise().query(`SELECT roles.title AS 'Job Title', roles.id AS ID, departments.dep_name AS Department, roles.salary as Salary
+    FROM roles
+    LEFT JOIN departments
+    ON roles.dep_id = departments.id;`);
+    this.displayTable(view);;       
+  }
+  async viewAllEmployees(){
+    let view = await db.promise().query(`SELECT employees.id AS ID, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', roles.title AS 'Job Title', departments.dep_name AS Department, roles.salary as Salary, concat(managers.first_name,' ',managers.last_name) AS Manager
+    FROM EMPLOYEES
+    LEFT JOIN roles
+    ON employees.role_id = roles.id
+    LEFT JOIN departments
+    ON roles.dep_id = departments.id
+    LEFT JOIN employees AS managers ON employees.manager_id = managers.id;`);
+    this.displayTable(view);      
+  }
+  async viewEmployeesByManager(){
+    let view = await db.promise().query(`SELECT employees.id AS ID, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', CONCAT (manager.first_name,' ',manager.last_name) AS Manager
+    FROM employees
+    LEFT JOIN employees AS manager
+    ON  employees.manager_id = manager.id
+    ORDER BY Manager`);
+    this.displayTable(view);      
+  }
+  async modify(element,response){
     let add;
     let view;
+    let findDeptCode;
+    let findRoleId;
+    let findManagerId;
+    let findEmployeeId;
+    let update;
     switch (element){
+
         case 'department':
+          //Query to add the new role in the roles table
           add = await db.promise().query(`INSERT INTO departments (dep_name)
           VALUES ('${response.deptName}')`);
           view = await db.promise().query(`SELECT departments.id as ID, departments.dep_name as Department FROM departments;`);
           console.log('\nDepartment added succesfully')
+
+          //Display the table with the new row
           this.displayTable(view);
         break;
+
         case 'role':
+          //Checks that enquirer is getting the options from the list
+          console.log(response.deptId)
+
+          //Look for the department ID code
+          findDeptCode = await db.promise().query(`SELECT id FROM departments WHERE dep_name='${response.deptId}'`);
+          findDeptCode = findDeptCode[0].map(element => element.id);
+
+          //Check if the department ID was found
+          console.log(findDeptCode);
+
+          //Query to add the new role in the roles table
           add = await db.promise().query(`INSERT INTO roles (title,salary,dep_id)
-          VALUES ('${response.roleTitle}',${response.salary},${response.deptId})`);
+          VALUES ('${response.roleTitle}',${response.salary},${findDeptCode})`);
           view = await db.promise().query(`SELECT roles.id as ID, roles.title as 'Job Title', roles.salary AS Salary, roles.dep_id AS 'Dept. ID' FROM roles;`);
           console.log('\nRole added succesfully')
+
+          //Display the table with the new row
           this.displayTable(view);
         break;
+
         case 'employee':
+          //Checks that enquirer is getting the options from the list
+          console.log(response.roleTitle)
+          console.log(response.managerName)
+
+          //Look for the role ID code & managers ID
+          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title='${response.roleTitle}'`);
+          findRoleId = findRoleId[0].map(element => element.id);
+          findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.managerName}'`);
+          findManagerId = findManagerId[0].map(element => element.id);
+
+          //Check if the role ID code & managers ID were found
+          console.log(findRoleId);
+          console.log(findManagerId);
+
+          //Query to add the new employee in the employees table
           add = await db.promise().query(`INSERT INTO employees (first_name,last_name,role_id,manager_id)
-          VALUES ('${response.firstName}',${response.lastName},${response.roleID},${response.managerId})`);
+          VALUES ('${response.firstName}','${response.lastName}',${findRoleId},${findManagerId})`);
           view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees;`);
           console.log('\nEmployee added succesfully')
+
+          //Display the table with the new row
           this.displayTable(view);
         break;
+
+        case 'Update role':
+          //Look for the role ID code & employee ID
+          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title='${response.newRole}'`);
+          findRoleId = findRoleId[0].map(element => element.id);
+          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.employeeName}'`);
+          findEmployeeId = findEmployeeId[0].map(element => element.id);
+
+          //Check if the role ID code & managers ID were found
+          console.log(findRoleId);
+          console.log(findEmployeeId);
+
+          //Query to update an employee role in the employees table
+          update = await db.promise().query(`UPDATE employees SET role_id = ${findRoleId} WHERE id=${findEmployeeId};`);
+          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id=${findEmployeeId};`);
+          console.log('\nEmployee role updated succesfully')
+
+          //Display the row with the update
+          this.displayTable(view);
+          break;
+
+        case 'Update manager':
+          //Look for the role ID code & employee ID
+          findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.newManager}'`);
+          findManagerId = findManagerId[0].map(element => element.id);
+          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.employeeName}'`);
+          findEmployeeId = findEmployeeId[0].map(element => element.id);
+
+          //Check if the role ID code & managers ID were found
+          console.log(findManagerId);
+          console.log(findEmployeeId);
+
+          //Query to update an employee role in the employees table
+          update = await db.promise().query(`UPDATE employees SET manager_id = ${findManagerId} WHERE id=${findEmployeeId};`);
+          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id=${findEmployeeId};`);
+          console.log('\nEmployee manager updated succesfully')
+
+          //Display the row with the update
+          this.displayTable(view);
+          break;
+
         default:
           console.log('Wrong option');
         break;
     } 
   }
-
 }
-1
 module.exports = { db, Query };
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const inquirer = require('inquirer');
 const {db, Query } = require('./query');
 const q = new Query();
 
-
 const menuQ = [
     {
         type:    'list',
         name:    'selection',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role','Exit'],
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Update an employee manager', 'View employees by manager', 'Exit'],
     }
 ];
 const deptQ = [
@@ -17,49 +16,6 @@ const deptQ = [
         message: 'Insert the name of the new Department:',
     }
 ];
-const roleQ = [
-    {
-        type:    'input',
-        name:    'roleTitle',
-        message: 'Insert the new role\'s title:',
-    },
-    {
-        type:    'input',
-        name:    'salary',
-        message: 'Insert the new role\'s salary:',
-    },
-    {
-        type:    'list',
-        name:    'deptId',
-        message: 'Select the new role\'s department ID:',
-        choices: dbOptions = db.query(`SELECT * FROM departments`, function (error, resp){
-            return resp;
-        }),
-    },
-];
-const employeeQ = [
-    {
-        type:    'input',
-        name:    'roleTitle',
-        message: 'Insert the new role\'s title:',
-    },
-    {
-        type:    'input',
-        name:    'salary',
-        message: 'Insert the new role\'s salary:',
-    },
-    {
-        type:    'input',
-        name:    'deptId',
-        message: 'Insert the new role\'s department ID:',
-    },
-    {
-        type:    'input',
-        name:    'deptId',
-        message: 'Insert the new role\'s department ID:',
-    },
-];
-
 menu = async () => {
     const option = await inquirer.prompt(menuQ);
     await switchOptions(option.selection);
@@ -67,33 +23,133 @@ menu = async () => {
 
 switchOptions = async (response) => {
     let answers;
+    let managerName;
+    let roleTitle;
+    let employeeName;
     switch (response){
+
         case 'View all departments':
             await q.viewAllDepartments();
             break;
+
         case 'View all roles':
             await q.viewAllRoles();
             break;
+
         case 'View all employees':
             await q.viewAllEmployees();
             break;
+
         case 'Add a department':
             answers = await inquirer.prompt(deptQ);
-            await q.addNew('department',answers);
+            await q.modify('department',answers);
             break;
+
         case 'Add a role':
+            const deptName = await getList('deptId')
+            console.log(deptName);
+            const roleQ = [
+                {
+                    type:    'input',
+                    name:    'roleTitle',
+                    message: 'Insert the new role\'s title:',
+                },
+                {
+                    type:    'input',
+                    name:    'salary',
+                    message: 'Insert the new role\'s salary:',
+                },
+                {
+                    type:    'list',
+                    name:    'deptId',
+                    message: 'Select the new role\'s department ID:',
+                    choices: deptName,
+                },
+            ];
             answers = await inquirer.prompt(roleQ);
-            await q.addNew('role',answers);
+            await q.modify('role',answers);
             break;
+
         case 'Add an employee':
-            answers = await inquirer.prompt(roleQ);
-            await q.addNew('employee',answers);
+            roleTitle = await getList('roleId');
+            console.log(roleTitle);
+            managerName = await getList('managerId');
+            console.log(managerName);
+            const employeeQ = [
+                {
+                    type:    'input',
+                    name:    'firstName',
+                    message: 'Employee\'s first name:',
+                },
+                {
+                    type:    'input',
+                    name:    'lastName',
+                    message: 'Employee\'s last name:',
+                },
+                {
+                    type:    'list',
+                    name:    'roleTitle',
+                    message: 'Select employee\s role:',
+                    choices: roleTitle,
+                },
+                {
+                    type:    'list',
+                    name:    'managerName',
+                    message: 'Select employee\s manager:',
+                    choices: managerName,
+                },
+            ];
+            
+            answers = await inquirer.prompt(employeeQ);
+            await q.modify('employee',answers);
             break;
+
         case 'Update an employee role':
+            employeeName = await getList('employeeName');
+            console.log(employeeName);
+            roleTitle = await getList('roleId');
+            console.log(roleTitle);
+            const employeeUpdateRole = [
+                {
+                    type:    'list',
+                    name:    'employeeName',
+                    message: 'Which employee do you want to update?',
+                    choices:  employeeName,
+                },
+                {
+                    type:    'list',
+                    name:    'newRole',
+                    message: 'Select their new role',
+                    choices:  roleTitle,
+                },
+            ];
+            answers = await inquirer.prompt(employeeUpdateRole);
+            await q.modify('Update role',answers);
             break;
-        case 'Update employee managers':
+        case 'Update an employee manager':
+            employeeName = await getList('employeeName');
+            console.log(employeeName);
+            managerName = await getList('managerId');
+            console.log(managerName);
+            const employeeUpdateManager = [
+                {
+                    type:    'list',
+                    name:    'employeeName',
+                    message: 'Which employee do you want to update?',
+                    choices:  employeeName,
+                },
+                {
+                    type:    'list',
+                    name:    'newManager',
+                    message: 'Select their new manager',
+                    choices:  managerName,
+                },
+            ];
+            answers = await inquirer.prompt(employeeUpdateManager);
+            await q.modify('Update manager',answers);
             break;
         case 'View employees by manager':
+            await q.viewEmployeesByManager();
             break;
         case 'View employees by department':
             break;
@@ -111,9 +167,21 @@ switchOptions = async (response) => {
    menu();
 };
 menu();
-// async function test (){
 
-// console.log((await db.promise().query(`SELECT * FROM departments`))[0]);
-// }
-
-//    test();
+getList = async (option) => {
+    switch (option){
+        case 'deptId':
+            return (await db.promise().query('SELECT dep_name FROM departments'))[0].map((element) => element.dep_name);
+            break;
+        case 'roleId':
+            return (await db.promise().query('SELECT title FROM roles'))[0].map((element) => element.title);
+            break;
+        case 'managerId':
+            return (await db.promise().query(`SELECT concat(employees.first_name,' ',employees.last_name) AS Manager FROM employees WHERE manager_id IS NULL`))[0].map((element) => element.Manager);
+            break;
+        case 'employeeName':
+            return (await db.promise().query(`SELECT concat(employees.first_name,' ',employees.last_name) AS 'Name' FROM employees`))[0].map((element) => element.Name);
+            break;
+        
+    }
+}
