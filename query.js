06f4753e3cd4ce4c71a47a5061499082cd2f8ba7
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-07-23 12:34:23 -0400

    Fixing injection risk

--- query.js
+++ query.js
@@ -67,7 +67,7 @@ class Query{
     LEFT JOIN departments
     ON departments.id = roles.dep_id
     ORDER BY ID) AS test
-    WHERE test.Dept = '${response.departmentBudget}';`);
+    WHERE test.Dept = ?;`,response.departmentBudget);
     // console.log(sum)
     this.displayTable(sum);      
   }
@@ -97,7 +97,7 @@ class Query{
           // console.log(response.deptId)
 
           //Look for the department ID code
-          findDeptCode = await db.promise().query(`SELECT id FROM departments WHERE dep_name='${response.deptId}'`);
+          findDeptCode = await db.promise().query(`SELECT id FROM departments WHERE dep_name= ?;`,response.deptId);
           findDeptCode = findDeptCode[0].map(element => element.id);
 
           //Check if the department ID was found
@@ -105,7 +105,7 @@ class Query{
 
           //Query to add the new role in the roles table
           add = await db.promise().query(`INSERT INTO roles (title,salary,dep_id)
-          VALUES (?, ? ,? )`, [response.roleTitle, response.salary, findDeptCode]);
+          VALUES (?, ? ,? );`, [response.roleTitle, response.salary, findDeptCode]);
           view = await db.promise().query(`SELECT roles.id as ID, roles.title as 'Job Title', roles.salary AS Salary, roles.dep_id AS 'Dept. ID' FROM roles;`);
           console.log('\nRole added succesfully')
 
@@ -119,10 +119,10 @@ class Query{
           // console.log(response.managerName)
 
           //Look for the role ID code & managers ID
-          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title='${response.roleTitle}'`);
+          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title= ?;`,response.roleTitle);
           findRoleId = findRoleId[0].map(element => element.id);
           if (response.managerName !== 'NULL'){
-            findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.managerName}'`);
+            findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)= ?;`,response.managerName);
             findManagerId = findManagerId[0].map(element => element.id);
           } else {
             findManagerId = 'NULL';
@@ -134,7 +134,7 @@ class Query{
 
           //Query to add the new employee in the employees table
           add = await db.promise().query(`INSERT INTO employees (first_name,last_name,role_id,manager_id)
-          VALUES ('${response.firstName}','${response.lastName}',${findRoleId},${findManagerId})`);
+          VALUES (?, ?, ?, ?)`, [response.firstName, response.lastName, findRoleId, findManagerId]);
           view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees;`);
           console.log('\nEmployee added succesfully')
 
@@ -144,9 +144,9 @@ class Query{
 
         case 'Update role':
           //Look for the role ID code & employee ID
-          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title='${response.newRole}';`);
+          findRoleId = await db.promise().query(`SELECT id FROM roles WHERE title= ?;`, response.newRole);
           findRoleId = findRoleId[0].map(element => element.id);
-          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.employeeName}';`);
+          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)= ?;`, response.employeeName);
           findEmployeeId = findEmployeeId[0].map(element => element.id);
 
           //Check if the role ID code & managers ID were found
@@ -154,8 +154,8 @@ class Query{
           // console.log(findEmployeeId);
 
           //Query to update an employee role in the employees table
-          update = await db.promise().query(`UPDATE employees SET role_id = ${findRoleId} WHERE id=${findEmployeeId};`);
-          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id=${findEmployeeId};`);
+          update = await db.promise().query(`UPDATE employees SET role_id = ? WHERE id= ?;`,[findRoleId,findEmployeeId]);
+          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id= ?;`,findEmployeeId);
           console.log('\nEmployee role updated succesfully')
 
           //Display the row with the update
@@ -165,12 +165,13 @@ class Query{
         case 'Update manager':
           //Look for the manager ID & employee ID
           if (response.newManager !== 'NULL'){
-            findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.newManager}';`);
+            findManagerId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)= ?;`,response.newManager);
+            // console.log(findManagerId[0][0].id);
             findManagerId = findManagerId[0].map(element => element.id);
           } else {
             findManagerId = 'NULL';
           }
-          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)='${response.employeeName}'`);
+          findEmployeeId = await db.promise().query(`SELECT id FROM employees WHERE concat(employees.first_name,' ',employees.last_name)= ?;`,response.employeeName);
           findEmployeeId = findEmployeeId[0].map(element => element.id);
 
           //Check if the managers ID & employee ID were found
@@ -178,8 +179,8 @@ class Query{
           // console.log(findEmployeeId);
 
           //Query to update an employee role in the employees table
-          update = await db.promise().query(`UPDATE employees SET manager_id = ${findManagerId} WHERE id=${findEmployeeId};`);
-          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id=${findEmployeeId};`);
+          update = await db.promise().query(`UPDATE employees SET manager_id = ? WHERE id= ?;`,[findManagerId,findEmployeeId]);
+          view = await db.promise().query(`SELECT employees.id as ID, employees.first_name as 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees WHERE id= ?;`,findEmployeeId);
           console.log('\nEmployee manager updated succesfully')
 
           //Display the row with the update
@@ -188,7 +189,7 @@ class Query{
 
         case 'Delete departments':
           //Query to delete a department from departments table
-          update = await db.promise().query(`DELETE FROM departments WHERE dep_name = '${response.department}';`);
+          update = await db.promise().query(`DELETE FROM departments WHERE dep_name = ?;`,response.department);
           view = await db.promise().query(`SELECT departments.id AS ID, departments.dep_name AS 'Department Name' FROM departments;`);
           console.log('\nDepartment deleted succesfully')
 
@@ -198,7 +199,7 @@ class Query{
         
         case 'Delete roles':
           //Query to delete a department from departments table
-          update = await db.promise().query(`DELETE FROM roles WHERE roles.title = '${response.role}';`);
+          update = await db.promise().query(`DELETE FROM roles WHERE roles.title = ?;`,response.role);
           view = await db.promise().query(`SELECT roles.id AS ID, roles.title AS 'Job Title', roles.salary AS Salary, roles.dep_id AS 'Dept. ID' FROM roles;`);
           console.log('\nRole deleted succesfully')
 
@@ -208,7 +209,7 @@ class Query{
 
         case 'Delete employees':
           //Query to delete a department from departments table
-          update = await db.promise().query(`DELETE FROM employees WHERE concat(employees.first_name,' ',employees.last_name) = '${response.employee}';`);
+          update = await db.promise().query(`DELETE FROM employees WHERE concat(employees.first_name,' ',employees.last_name) = ?;`,response.employee);
           view = await db.promise().query(`SELECT employees.id AS ID, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', employees.role_id AS 'Role ID', employees.manager_id AS 'Manager ID' FROM employees;`);
           console.log('\nEmployee deleted succesfully')
 
